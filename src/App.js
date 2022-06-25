79fb0cb5101e22bd67fdadeae7e3a9160fd92c62
a2 <dev.monsalve@yahoo.com>
2022-06-25 12:48:50 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hola Mundo!
+    </div>
+  );
+}
+
+export default App;
